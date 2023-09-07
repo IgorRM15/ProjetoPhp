b56762faad054806ca1d62e2e05a458a3a9438c0
iguinho696 <i.rocha.martins10@gmail.com>
2023-09-07 02:00:25 -0300

    Projeto Php

---
+++ src/main.php
@@ -0,0 +1,8 @@
+<?php
+ 
+ include_once __DIR__. "/../vendor/autoload.php";
+
+ use App\model\pessoa;
+ 
+ $pessoa = new Pessoa();
+ var_dump ($pessoa);
---
+++ src/model/Pessoa.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\model;
+
+class Pessoa {
+     public $nome = "Igor Rocha";
+     private $idade = "17";
+ }
